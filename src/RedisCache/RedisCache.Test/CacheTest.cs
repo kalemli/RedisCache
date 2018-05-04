b6918543afab1bf61b25b88eec0c4195bc721ed3
Atif Galamov <kalemli@gmail.com>
2018-05-04 17:44:16 +0400

    RedisCache.Test project added

---
+++ src/RedisCache/RedisCache.Test/CacheTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RedisCache.Test
+{
+    [TestClass]
+    public class CacheTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
