d63e3ebb17fd70146ca087c2f11cd2a626587f75
Atif Galamov <kalemli@gmail.com>
2018-09-11 18:03:18 +0400

    CacheUtil and Unit Tests

--- src/RedisCache/RedisCache.Test/CacheTest.cs
+++ src/RedisCache/RedisCache.Test/CacheTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace RedisCache.Test
@@ -7,8 +9,66 @@ namespace RedisCache.Test
     public class CacheTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SingleObjectTest()
         {
+            string key = "test";
+            int value = new Random().Next(1, 100);
+            CacheUtil.Save(key, value);
+
+            int cachedValue = CacheUtil.Get<int>(key);
+            Assert.AreEqual(value, cachedValue);
         }
+
+        [TestMethod]
+        public void ObjectTest()
+        {
+            string key = "person";
+            Person person = new Person
+            {
+                FirstName = "Atif",
+                Surname = "Qələmov",
+                Age = 35,
+                BirthDate = new DateTime(1982, 10, 27)
+            };
+            CacheUtil.Save(key, person);
+
+            Person cachedPerson = CacheUtil.Get<Person>(key);
+            Assert.AreEqual(person.FirstName, cachedPerson.FirstName);
+            Assert.AreEqual(person.Surname, cachedPerson.Surname);
+            Assert.AreEqual(person.Age, cachedPerson.Age);
+            Assert.AreEqual(person.BirthDate, cachedPerson.BirthDate);
+        }
+
+        [TestMethod]
+        public void CollectionTest()
+        {
+            string key = "list";
+            List<Person> list = new List<Person>();
+
+            for (int i = 0; i < 10; i++)
+                list.Add(
+                    new Person
+                    {
+                        FirstName = $"Atif - {i}",
+                        Surname = $"Qələmov - {i}",
+                        Age = 35 + i,
+                        BirthDate = new DateTime(1982, 10, i + 1)
+                    });
+            CacheUtil.Save(key, list);
+
+            var s = Stopwatch.StartNew();
+            List<Person> cachedList = CacheUtil.Get<List<Person>>(key);
+            s.Stop();
+            string seconds = s.Elapsed.TotalSeconds.ToString();
+            Assert.AreEqual(list.Count, cachedList.Count);
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public DateTime BirthDate { get; set; }
     }
 }
---
+++ src/RedisCache/RedisCache/CacheUtil.cs
@@ -0,0 +1,31 @@
+﻿using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisCache
+{
+    public class CacheUtil
+    {
+        private static string Host = "localhost";
+        private static int Port = 6379;
+
+        public static void Save(string key, object value)
+        {
+            using (var client = new RedisClient(Host, Port))
+            {
+                client.Set(key, value);
+            }
+        }
+
+        public static T Get<T>(string key)
+        {
+            using (var client = new RedisClient(Host, Port))
+            {
+                return client.Get<T>(key);
+            }
+        }
+    }
+}
